32810a658def5a04bd2577b23761e644f20ad7cf
Camille <25295489+Campalo@users.noreply.github.com>
2019-02-18 19:04:17 +0100

    add Functions changeDay + tomorrowDay

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-const today = "Monday";
+let today = "Monday";
 let tomorrow = "";
 
+// fonction qui récupère l'id de l'élément TodayName et assigne sa valeur à la variable today, qui prend alors une nouvelle valeur
+// puis déclenche la fonction tomorrowDay pour attribuer une valeur à la variable tomorrow
+function changeDay() {
+    today = document.getElementById('todayName').value;
+    console.log(`Today is ${today}`);
+    tomorrowDay();
+}
+
+function tomorrowDay() {
+    // solution with SWITCH
+    switch (today) {
+        case "Monday":
+            tomorrow = "Tuesday";
+            break;
+        case "Tuesday":
+            tomorrow = "Wednesday";
+            break;
+        case "Wednesday":
+            tomorrow = "Thursday";
+            break;
+        case "Thursday":
+            tomorrow = "Friday";
+            break;
+        case "Friday":
+            tomorrow = "Saturday";
+            break;
+        case "Saturday":
+            tomorrow = "Sunday";
+            break;
+        case "Sunday":
+            tomorrow = "Monday";
+            break;
+        default:
+            console.log("Unknown day")
+    }
+
+    if (tomorrow !== "") {
+        console.log(`Tomorrow will be ${tomorrow}`);
+    }
+
+}
+
 // solution with IF ELSE
+/*
 if (today === "Monday") {
     tomorrow = "Tuesday";
 } else if (today === "Tuesday") {
@@ -22,37 +65,5 @@ if (today === "Monday") {
 
 if (tomorrow !== "") {
     console.log(`Tomorrow will be ${tomorow}`);
-}
-
-// solution with SWITCH
-/* 
-switch (today) {
-    case "Monday":
-    tomorrow = "Tuesday";
-    break;
-    case "Tuesday":
-    tomorrow = "Wednesday";
-    break;
-    case "Wednesday":
-    tomorrow = "Thursday";
-    break;
-    case "Thursday":
-    tomorrow = "Friday";
-    break;
-    case "Friday":
-    tomorrow = "Saturday";
-    break;
-    case "Saturday":
-    tomorrow = "Sunday";
-    break;
-    case "Sunday":
-    tomorrow = "Monday";
-    break;
-    default:
-    console.log ("Unknown day")
-}
-
-if (tomorrow !== ""){
-    console.log(`Tomorrow will be ${tomorrow}`);
 }
 */
